9c7a654875709b5ba3bdac0da116d8fd6fa215d6
tmaji12 <tushar.maji123@gmail.com>
2019-10-23 17:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:Red;">Welcome to test app!!</h1> \n');
+  res.send('<h1 style="color:Green;">Welcome to test app!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
